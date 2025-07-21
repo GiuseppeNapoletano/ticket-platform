e7ed1b124735caa1f215ab343b512a962b58f8bd
GiuseppeNapoletano <andreanap04@gmail.com>
2025-07-21 20:59:57 +0200

    feat: db

---
+++ src/main/java/org/java/milestone/ticket_platform/repository/RoleRepository.java
@@ -0,0 +1,8 @@
+package org.java.milestone.ticket_platform.repository;
+
+import org.java.milestone.ticket_platform.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository <Role, Integer>{
+    
+}
